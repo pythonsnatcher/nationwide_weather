9329725affd39a3e4755783885fb985e79481d74
pythonsnatcher <pythonsnatcher@gmail.com>
2025-01-05 18:25:20 +0000

    Update deploy_app.R

--- scripts/deploy_app.R
+++ scripts/deploy_app.R
@@ -1,24 +1,20 @@
-# Ensure required packages are installed
-required_packages <- c("shiny", "rsconnect", "httpuv", "rmarkdown", "knitr", "jsonlite", "RJSONIO", "htmltools", "reticulate")
-
-new_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
-if(length(new_packages)) install.packages(new_packages, repos = "https://cran.rstudio.com/")
-
-
 # Load the rsconnect package
+if (!require("rsconnect")) {
+  install.packages("rsconnect", repos = "https://cran.rstudio.com/")
+}
 library(rsconnect)
 
 # Set account information
 rsconnect::setAccountInfo(
-  name = 'tuco-bear',  # Replace with your ShinyApps.io account name
+  name = 'tuco-bear',
   token = Sys.getenv("SHINYAPPS_TOKEN"),
   secret = Sys.getenv("SHINYAPPS_SECRET")
 )
 
 # Deploy the app
 rsconnect::deployApp(
-  appDir = "scripts",      # Path to your app directory
-  appFiles = c("app.r"),   # Ensure this includes all necessary files for deployment
-  appName = "your-app-name",  # Replace with your app name
+  appDir = "scripts",
+  appFiles = c("app.r"),
+  appName = "your-app-name",
   forceUpdate = TRUE
 )
